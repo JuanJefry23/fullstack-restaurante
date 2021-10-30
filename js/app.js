d81b2a02b6f8ccc9afa262e8dc1acd0eb4dd2fee
JJOA <og.jjoa@gmail.com>
2021-10-30 20:33:34 -0300

    Adding Data

---
+++ js/app.js
@@ -0,0 +1,498 @@
+//Objeto que almacenará todo los datos del cliente
+let cliente = {
+  mesa: "",
+  hora: "",
+  pedido: [],
+};
+
+const categorias = {
+  1: "Comida",
+  2: "Bebidas",
+  3: "Postres",
+};
+
+const btnGuardarCliente = document.querySelector("#guardar-cliente");
+btnGuardarCliente.addEventListener("click", guardarCliente);
+
+function guardarCliente() {
+  //Obtenemos los valores de los inputs
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
+
+  //Revisamos si hay al menos un campo vacío
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
+
+  if (camposVacios) {
+    const existeAlerta = document.querySelector(".invalid-feedback");
+
+    if (!existeAlerta) {
+      const alerta = document.createElement("div");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
+      document.querySelector(".modal-body form").appendChild(alerta);
+
+      //Eliminar la alerta
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+
+    return;
+  }
+
+  //Asignamos datos del formulario al cliente (*1)
+  cliente = { ...cliente, mesa, hora };
+
+  //Ocultar Modal
+  const modalFormulario = document.querySelector("#formulario");
+  const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+  modalBootstrap.hide();
+
+  //Mostrar las secciones, mostramos las secciones una vez se haya completado el formulario correctamente!
+  mostrarSecciones();
+
+  //Obtenemos los platilos desde nuestra "db.json"
+  obtenerPlatillos();
+}
+
+async function obtenerPlatillos() {
+  const url = "https://restaurante-platillos.herokuapp.com//platillos";
+
+  const respuesta = await fetch(url);
+  const resultado = await respuesta.json();
+  mostrarPlatillos(resultado)
+
+  /*fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => mostrarPlatillos(resultado))
+    .catch((error) => console.log(error));
+    */
+}
+
+//Hacemos scripting (*2)
+function mostrarPlatillos(platillos) {
+  const contenido = document.querySelector("#platillos .contenido");
+
+  platillos.forEach((platillo) => {
+    const row = document.createElement("div");
+    row.classList.add("row", "py-3", "border-top");
+
+    const nombre = document.createElement("div");
+    nombre.classList.add("col-md-4");
+    nombre.textContent = platillo.nombre;
+
+    const precio = document.createElement("div");
+    precio.classList.add("col-md-3", "fw-bold");
+    precio.textContent = `$${platillo.precio}`;
+
+    const categoria = document.createElement("div");
+    categoria.classList.add("col-md-3");
+    categoria.textContent = categorias[platillo.categoria];
+
+    const inputCantidad = document.createElement("input");
+    inputCantidad.type = "number";
+    inputCantidad.min = 0;
+    inputCantidad.value = 0;
+    inputCantidad.id = `producto-${platillo.id}`;
+    inputCantidad.classList.add("form-control"); //Agrego la clase de bootstrap para los "inputs" , muy importante !
+
+    //Función que detecta la cantidad y el platillo que se está agregando
+    inputCantidad.onchange = function () {
+      const cantidad = parseInt(inputCantidad.value);
+      agregarPlatillo({ ...platillo, cantidad });
+    };
+
+    const agregar = document.createElement("div");
+    agregar.classList.add("col-md-2");
+    agregar.appendChild(inputCantidad);
+
+    row.appendChild(nombre);
+    row.appendChild(precio);
+    row.appendChild(categoria);
+    row.appendChild(agregar);
+
+    contenido.appendChild(row);
+  });
+}
+
+function mostrarSecciones() {
+  //Selecciono todas las secciones ocultas (que tengan el display none de bootstrap: "d-none")
+  const seccionesOcultas = document.querySelectorAll(".d-none");
+
+  //Las recorro con un "forEach" removiendo la clase "d-none" de cada una de ellas para que se puedan visualizar
+  seccionesOcultas.forEach((seccion) => seccion.classList.remove("d-none"));
+}
+
+function agregarPlatillo(producto) {
+  //Extraer el pedido actual
+  let { pedido } = cliente;
+
+  //Revisar que la cantidad sea mayor a 0
+  if (producto.cantidad > 0) {
+    //Comprueba si el elemento ya existe en el array
+    if (pedido.some((articulo) => articulo.id === producto.id)) {
+      //El articulo ya existe, Actualizar la cantidad
+      const pedidoActualizado = pedido.map((articulo) => {
+        if (articulo.id === producto.id) {
+          articulo.cantidad = producto.cantidad;
+        }
+        return articulo;
+      });
+
+      //Se asigna el nuevo array a cliente.pedido
+      cliente.pedido = [...pedidoActualizado];
+    } else {
+      //El articulo no existe, lo agregamos al array de pedido
+      cliente.pedido = [...pedido, producto];
+    }
+  } else {
+    //Eliminar elementos cuando la cantidad es 0
+    const resultado = pedido.filter((articulo) => articulo.id !== producto.id);
+    cliente.pedido = [...resultado];
+  }
+
+  //Limpiar el código HTML previo
+  limpiarHTML();
+
+  if (cliente.pedido.length) {
+    //Mostrar el resumen
+    actualizarResumen();
+  } else {
+    mensajePedidoVacio();
+  }
+}
+
+function actualizarResumen() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const resumen = document.createElement("div");
+  resumen.classList.add("col-md-6", "card", "py-2", "px-3", "shadow");
+
+  //Información de la mesa
+  const mesa = document.createElement("p");
+  mesa.textContent = "Mesa: ";
+  mesa.classList.add("fw-bold");
+
+  const mesaSpan = document.createElement("span");
+  mesaSpan.textContent = cliente.mesa;
+  mesaSpan.classList.add("fw-normal");
+
+  //Información de la hora
+  const hora = document.createElement("p");
+  hora.textContent = "Hora: ";
+  hora.classList.add("fw-bold");
+
+  const horaSpan = document.createElement("span");
+  horaSpan.textContent = cliente.hora;
+  horaSpan.classList.add("fw-normal");
+
+  //Agregar a los elementos padre
+  mesa.appendChild(mesaSpan);
+  hora.appendChild(horaSpan);
+
+  //Título de la sección
+  const heading = document.createElement("h3");
+  heading.textContent = "Platillos consumidos";
+  heading.classList.add("my-4", "text-center");
+
+  //Iterar sobre el array de pedidos
+  const grupo = document.createElement("ul");
+  grupo.classList.add("list-group");
+
+  const { pedido } = cliente;
+  pedido.forEach((articulo) => {
+    const { nombre, cantidad, precio, id } = articulo;
+
+    const lista = document.createElement("li");
+    lista.classList.add("list-group-item");
+
+    const nombreEl = document.createElement("h4");
+    nombreEl.classList.add("my-4");
+    nombreEl.textContent = nombre;
+
+    //Cantidad del artículo
+    const cantidadEl = document.createElement("p");
+    cantidadEl.classList.add("fw-bold");
+    cantidadEl.textContent = "Cantidad: ";
+
+    const cantidadValor = document.createElement("span");
+    cantidadValor.classList.add("fw-normal");
+    cantidadValor.textContent = cantidad;
+
+    //Precio del artículo
+    const precioEl = document.createElement("p");
+    precioEl.classList.add("fw-bold");
+    precioEl.textContent = "Precio: ";
+
+    const precioValor = document.createElement("span");
+    precioValor.classList.add("fw-normal");
+    precioValor.textContent = `$${precio}`;
+
+    //Subtotal del artículo
+    const subtotalEl = document.createElement("p");
+    subtotalEl.classList.add("fw-bold");
+    subtotalEl.textContent = "Subtotal: ";
+
+    const subtotalValor = document.createElement("span");
+    subtotalValor.classList.add("fw-normal");
+    subtotalValor.textContent = calcularSubtotal(precio, cantidad);
+
+    //Botón para eliminar
+    const btnEliminar = document.createElement("button");
+    btnEliminar.classList.add("btn", "btn-danger");
+    btnEliminar.textContent = "Eliminar del Pedido";
+
+    //Función para eliminar del pedido
+    btnEliminar.onclick = function () {
+      eliminarProducto(id);
+    };
+
+    //Agregar valores a sus contenedores
+    cantidadEl.appendChild(cantidadValor);
+    precioEl.appendChild(precioValor);
+    subtotalEl.appendChild(subtotalValor);
+
+    //Agregar elementos al LI
+    lista.appendChild(nombreEl);
+    lista.appendChild(cantidadEl);
+    lista.appendChild(precioEl);
+    lista.appendChild(subtotalEl);
+    lista.appendChild(btnEliminar);
+
+    //Agregar lista al grupo principal
+    grupo.appendChild(lista);
+  });
+
+  //Agregar al contenido
+  resumen.appendChild(heading);
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(grupo);
+
+  contenido.appendChild(resumen);
+
+  //Mostrar Formulario de Propinas
+  formularioPropinas();
+}
+
+function limpiarHTML() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  while (contenido.firstChild) {
+    contenido.removeChild(contenido.firstChild);
+  }
+}
+
+function calcularSubtotal(precio, cantidad) {
+  return `$${precio * cantidad}`;
+}
+
+function eliminarProducto(id) {
+  const { pedido } = cliente;
+  const resultado = pedido.filter((articulo) => articulo.id !== id);
+  cliente.pedido = [...resultado];
+
+  //Limpiar el código HTML previo
+  limpiarHTML();
+
+  if (cliente.pedido.length) {
+    //Mostrar el resumen
+    actualizarResumen();
+  } else {
+    mensajePedidoVacio();
+  }
+
+  //El producto se eliminó por lo tanto regresamos la cantidad a 0 en el formulario
+  const productoEliminado = `#producto-${id}`;
+  const inputEliminado = document.querySelector(productoEliminado);
+  inputEliminado.value = 0;
+}
+
+function mensajePedidoVacio() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const texto = document.createElement("p");
+  texto.classList.add("text-center");
+  texto.textContent = "Añade los elementos del pedido";
+
+  contenido.appendChild(texto);
+}
+
+function formularioPropinas() {
+  const contenido = document.querySelector("#resumen .contenido");
+
+  const formulario = document.createElement("div");
+  formulario.classList.add("col-md-6", "formulario");
+
+  const divFormulario = document.createElement("div");
+  divFormulario.classList.add("card", "py-2", "px-3", "shadow");
+
+  const heading = document.createElement("h3");
+  heading.classList.add("my-4", "text-center");
+  heading.textContent = "Propina";
+
+  //Radio Button 10%
+  const radio10 = document.createElement("input");
+  radio10.type = "radio";
+  radio10.name = "propina";
+  radio10.value = "10";
+  radio10.classList.add("form-check-inpuit");
+  radio10.onclick = calcularPropina;
+
+  const radio10Label = document.createElement("label");
+  radio10Label.textContent = "10%";
+  radio10Label.classList.add("form-check-label");
+
+  const radio10Div = document.createElement("div");
+  radio10Div.classList.add("form-check");
+
+  radio10Div.appendChild(radio10);
+  radio10Div.appendChild(radio10Label);
+
+  //Radio Button 25%
+  const radio25 = document.createElement("input");
+  radio25.type = "radio";
+  radio25.name = "propina";
+  radio25.value = "25";
+  radio25.classList.add("form-check-inpuit");
+  radio25.onclick = calcularPropina;
+
+  const radio25Label = document.createElement("label");
+  radio25Label.textContent = "25%";
+  radio25Label.classList.add("form-check-label");
+
+  const radio25Div = document.createElement("div");
+  radio25Div.classList.add("form-check");
+
+  radio25Div.appendChild(radio25);
+  radio25Div.appendChild(radio25Label);
+
+  //Radio Button 50%
+  const radio50 = document.createElement("input");
+  radio50.type = "radio";
+  radio50.name = "propina";
+  radio50.value = "50";
+  radio50.classList.add("form-check-inpuit");
+  radio50.onclick = calcularPropina;
+
+  const radio50Label = document.createElement("label");
+  radio50Label.textContent = "50%";
+  radio50Label.classList.add("form-check-label");
+
+  const radio50Div = document.createElement("div");
+  radio50Div.classList.add("form-check");
+
+  radio50Div.appendChild(radio50);
+  radio50Div.appendChild(radio50Label);
+
+  //Agregar al Div principal
+  divFormulario.appendChild(heading);
+  divFormulario.appendChild(radio10Div);
+  divFormulario.appendChild(radio25Div);
+  divFormulario.appendChild(radio50Div);
+
+  //Agregar al Formulario
+  formulario.appendChild(divFormulario);
+
+  contenido.appendChild(formulario);
+}
+
+function calcularPropina() {
+  const { pedido } = cliente;
+  let subtotal = 0;
+
+  //Calcular el Subtotal a pagar
+  pedido.forEach((articulo) => {
+    subtotal += articulo.cantidad * articulo.precio;
+  });
+
+  //Seleccionamos un elemento segun su atributo "name" y leemos su valor !
+  //Seleccionar el Radio Button con la propina del cliente
+  const propinaSeleccionada = document.querySelector(
+    "[name='propina']:checked"
+  ).value;
+
+  //Calcular la propina
+  const propina = (subtotal * parseInt(propinaSeleccionada)) / 100;
+
+  //Calcular el total a pagar
+  const total = subtotal + propina;
+
+  mostrarTotalHTML(subtotal, total, propina);
+}
+
+function mostrarTotalHTML(subtotal, total, propina) {
+  const divTotales = document.createElement("div");
+  divTotales.classList.add("total-pagar", "my-5");
+
+  //Subtotal
+  const subtotalParrafo = document.createElement("p");
+  subtotalParrafo.classList.add("fs-4", "fw-bold", "mt-5");
+  subtotalParrafo.textContent = "Subtotal Consumo: ";
+
+  const subtotalSpan = document.createElement("span");
+  subtotalSpan.classList.add("fw-normal");
+  subtotalSpan.textContent = `$${subtotal}`;
+
+  subtotalParrafo.appendChild(subtotalSpan);
+
+  //Propina
+  const propinaParrafo = document.createElement("p");
+  propinaParrafo.classList.add("fs-4", "fw-bold", "mt-5");
+  propinaParrafo.textContent = "Propina: ";
+
+  const propinaSpan = document.createElement("span");
+  propinaSpan.classList.add("fw-normal");
+  propinaSpan.textContent = `$${propina}`;
+
+  propinaParrafo.appendChild(propinaSpan);
+
+  //Total
+  const totalParrafo = document.createElement("p");
+  totalParrafo.classList.add("fs-4", "fw-bold", "mt-5");
+  totalParrafo.textContent = "Total a Pagar: ";
+
+  const totalSpan = document.createElement("span");
+  totalSpan.classList.add("fw-normal");
+  totalSpan.textContent = `$${total}`;
+
+  totalParrafo.appendChild(totalSpan);
+
+  //Eliminar el último resultado
+  const totalpagarDiv = document.querySelector(".total-pagar");
+  if (totalpagarDiv) {
+    totalpagarDiv.remove();
+  }
+
+  divTotales.appendChild(subtotalParrafo);
+  divTotales.appendChild(propinaParrafo);
+  divTotales.appendChild(totalParrafo);
+
+  const formulario = document.querySelector(".formulario > div");
+  formulario.appendChild(divTotales);
+}
+
+
+
+/*
+
+-Comando para levantar el "json-server":
+json-server --watch db.json --port 4000
+
+-En (*1) si coloco "...cliente" al final del objeto o sea: {hora,mesa,...cliente} reescribiría todo los valores
+anteriores dejandolos en blanco, por eso al usar el spread lo uso al inicio para tener el arreglo pedido vacio para
+poder llenarlo luego.
+
+-Fetch API solo consume "textos o JSON" no consume otro tipo de datos.
+
+-(*2)Lo que estamos haciendo al crear los elementos, agregarles clases y appendChild a sus respectivos lugares se llama 
+scripting es más rápido y seguro que hacer innerHTML , aunque este último requiere menos código!
+
+-Recordar que los valores que leamos de un "input" serán "strings" por más que sean type="number" asi que tenemos que convertirlos
+a números en caso sea necesario !
+
+
+NOTA: Volver a practicar más el video 271 en la sección 40
+
+
+*/
